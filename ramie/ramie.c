04e26da7f94b9e8ca7abcd452e1999d41ee8e331
Harpinoth <mazsparrow@gmail.com>
2016-01-09 19:03:39 +0100

    A bit of wrestling with ramie.c
    Builder is crying over this, trying to solve, but majorly clueless where
    to start.

--- ramie/ramie.c
+++ ramie/ramie.c
@@ -1,1 +1,41 @@
+#include "../common/mem.h"
+#include "../common/log.h"
+#include "../common/net/net.h"
+#include "../common/net/client.h"
+#include "../common/packet.h"
+#include "ramie.h"
+#include "process.h"
 
+int main(void){
+    logCreate("ramie.log");
+    memCreate();
+    netCreate();
+
+    netClientCreate("127.0.0.1", 888, ramieOnConnect, ramieProcessPacket);
+    netRun();
+
+    netDestroy();
+    memDestroy();
+    logDestroy();
+    return 0;
+}
+
+void ramieOnConnect(tNetClient *pClient) {
+	tPacket sPacket;
+
+	// Send ID packet
+	logWrite(
+		"Sending client type: '%s\' (%u)",
+		g_szClientTypes[CLIENT_TYPE_RAMIE], CLIENT_TYPE_RAMIE
+	);
+
+	packetMakeSetType(&sPacket, CLIENT_TYPE_RAMIE);
+	netSend(&pClient->sSrvConn, &sPacket, netReadAfterWrite);
+}
+
+void ramieProcessPacket(tNetClientServer *pServer, tNetConn *pClient, tPacket *pPacket) {
+
+	switch(pPacket->sHead.ubType) {
+
+	}
+}
--- ramie/ramie.h
+++ ramie/ramie.h
@@ -1,1 +1,15 @@
+#ifndef GUARD_RAMIE_RAMIE_H
+#define GUARD_RAMIE_RAMIE_H
+
+void ramieProcessPacket(
+	IN tNetClientServer *pClientServer,
+	IN tNetConn *pClient,
+	IN tPacket *pPacket
+);
+
+void ramieOnConnect(
+	IN tNetClient *pClient
+);
+
+#endif // GUARD_RAMIE_RAMIE_H
 
